247d9ac1383c1d1291df104b700d4836b8570cd7
vieira <vini.clau0@gmail.com>
2023-03-01 18:11:18 -0300

    resolve 7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header';
+import Missions from './components/Missions';
 // import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <Missions />
+      </>
     );
   }
 }
